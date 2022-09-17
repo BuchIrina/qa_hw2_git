5a0eb89be0c89fe9494c5c97c9eff355eafd0b45
Irina Buchneva <buchneva.irina986@gmail.com>
2022-09-17 17:56:59 +0300

    first test

---
+++ src/test/java/PracticeFormTest.java
@@ -0,0 +1,52 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.w3c.dom.Text;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeFormTest {
+
+    @BeforeAll
+    static void config() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillPracticeFormTest() {
+        String firstName = "Irina";
+        String lastName = "Buchneva";
+        String email = "irina@gmail.com";
+        String mobile = "89243657856";
+        String subjects = "Eng";
+        String currentAddress = "Moscow, Pirogova street 15-56";
+
+
+        open("/automation-practice-form");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue(mobile);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("November");
+        $(".react-datepicker__year-select").selectOption("1986");
+        $(".react-datepicker__day--030").click();
+        $("#subjectsInput").setValue(subjects).pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFile();
+
+        $("#currentAddress").setValue(currentAddress);
+//        $("#firstName").setValue("Irina");
+//        $("#firstName").setValue("Irina");
+//        $("#firstName").setValue("Irina");
+//        $("#firstName").setValue("Irina");
+    }
+}
